d131ab7a4c9e260b4b1d31ac834088183978c0b8
Tom Shortridge <tomshortridge@toms-mbp-2.lan>
2023-10-26 13:13:22 +0100

    feat: adding basic pages

--- waggly-web/src/routes/+page.svelte
+++ waggly-web/src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script>
-    import { Alert } from 'flowbite-svelte';
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
   
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
+<div class="p-8">
+    <Navbar >
+        <NavBrand href="/">
+          <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Wagg.ly Logo" /> -->
+          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Wagg.ly</span>
+        </NavBrand>
+        <NavHamburger  />
+        <NavUl >
+          <NavLi href="/">Home</NavLi>
+          <NavLi href="/add-pet">Register a Pet</NavLi>
+          <NavLi href="/add-walker">Register a Walker</NavLi>
+        </NavUl>
+    </Navbar>
+</div>
+
+<div class="p-8">
+    <h1>Welcome to Wagg.ly</h1>
+</div>
---
+++ waggly-web/src/routes/add-walker/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { Button, Label, Input, Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+  
+  <div class="p-8">
+    <Navbar  >
+        <NavBrand href="/">
+          <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Wagg.ly Logo" /> -->
+          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Wagg.ly</span>
+        </NavBrand>
+        <NavHamburger  />
+        <NavUl >
+          <NavLi href="/">Home</NavLi>
+          <NavLi href="/add-pet">Register a Pet</NavLi>
+          <NavLi href="/add-walker">Register a Walker</NavLi>
+        </NavUl>
+      </Navbar>
+      <h1 class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Register a Walker</h1>
+      <div class="mb-6">
+        <Label>Walker Name</Label>
+        <Input id="walker-name"/>
+      </div>
+      <div class="mb-6">
+        <Label>Pet Owner Name</Label>
+        <Input id="pet-owner-name"/>
+      </div>
+      <div class="mb-6">
+        <Label>Walker Email</Label>
+        <Input id="walker-email"/>
+      </div>
+      <div class="mb-6">
+        <Label>Walker Contact Number</Label>
+        <Input id="walker-contact-number"/>
+      </div>
+      <div class="mb-6">
+        <Label>Walker Address</Label>
+        <Input id="walker-address"/>
+      </div>
+      <div>
+        <Button color="purple">Submit</Button>
+      </div>
+  </div>
